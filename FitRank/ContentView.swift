d65f2b6f5e4d61eb2a0f3c4c072bb48d4e3f2954
navraj213 <nsingh59@nyit.edu>
2025-06-07 01:53:04 -0400

    Initial Commit

---
+++ FitRank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FitRank
+//
+//  Created by Navraj Singh on 6/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world! This is FitRank")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FitRank/FitRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitRankApp.swift
+//  FitRank
+//
+//  Created by Navraj Singh on 6/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FitRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
